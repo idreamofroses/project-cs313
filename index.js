f18fc6c6f770d07ec18be25032cdcb5b375089db
idreamofroses <idreamofroses@gmail.com>
2017-03-23 19:09:24 -0600

    updated view groups page

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
 var url = require('url');
+var bodyParser = require('body-parser');
 var pg = require("pg"); // This is the postgres database connection module.
 const connectionString = "postgres://brother_burton:bradismyfavoritestudent@localhost:5432/classquiz";
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+//body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -17,9 +21,27 @@ app.get('/', function(request, response) {
 });
 
 app.get('/viewClasses', function(request, response) {
-    console.log("success!!");
+    console.log("view Classes called");
     viewClasses(request, response);
 });
+
+app.get('/viewGroups', function(request, response) {
+    console.log("view quizzes called");
+    viewGroups(request, response);
+    
+});
+
+app.get('/viewStudents', function(request, response) {
+    console.log("view students called");
+    viewStudents(request, response);
+    
+});
+
+app.post('/newClass', function(request, response) {
+    console.log("success!!");
+    insertClass(request, response);
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
@@ -36,9 +58,80 @@ function viewClasses(request, response) {
             callback(err, null);
         }
         
-        var sql = "SELECT * FROM class;"
+        var sql = "SELECT id, name, class_code, section FROM class WHERE teacher_id = $1::int;"
+        var params = [1 /*temporary until we know the teacher_id*/];
         
-        var query = client.query(sql, function(err, result){
+        var query = client.query(sql, params, function(err, result){
+            client.end(function(err) {
+                if (err) throw err;
+            });
+            
+            if (err) {
+                console.log("Error in query");
+                console.log(err);
+                callback(err, null);
+            }
+            
+            //console.log("Found result: " + JSON.stringify(result.rows));
+            
+            response.json(result.rows);
+        });
+    });
+}
+
+function viewGroups(request, response) {
+   var requestUrl = url.parse(request.url, true);
+   var classId = parseInt(requestUrl.query.classId);  
+    console.log("class id: " + classId);
+  
+    var client = new pg.Client(connectionString);
+    
+   client.connect(function(err){
+        if (err) {
+            console.log("Error connecting to database");
+            console.log(err);
+            callback(err, null);
+        }
+        
+        var sql = "SELECT cg.id, cg.group_name FROM classgroup cg INNER JOIN class c ON cg.class_id = c.id WHERE c.id = $1::int;";
+        var params = [classId];
+        
+        var query = client.query(sql, params, function(err, result){
+            client.end(function(err) {
+                if (err) throw err;
+            });
+            
+            if (err) {
+                console.log("Error in query");
+                console.log(err);
+                callback(err, null);
+            }
+            
+         //   console.log("Found result: " + JSON.stringify(result.rows));
+            
+            response.json(result.rows);
+        });
+    }); 
+}
+
+function viewStudents(request, response) {
+   var requestUrl = url.parse(request.url, true);
+   var groupId = parseInt(requestUrl.query.groupId);  
+    console.log("group id: " + groupId);
+  
+    var client = new pg.Client(connectionString);
+    
+   client.connect(function(err){
+        if (err) {
+            console.log("Error connecting to database");
+            console.log(err);
+            callback(err, null);
+        }
+        
+        var sql = "SELECT firstname, lastname FROM classuserTOgroup ctg INNER JOIN classUser cu ON cu.id = ctg.classUser_id WHERE ctg.group_id = $1::int;";
+        var params = [groupId];
+        
+        var query = client.query(sql, params, function(err, result){
             client.end(function(err) {
                 if (err) throw err;
             });
@@ -53,5 +146,43 @@ function viewClasses(request, response) {
             
             response.json(result.rows);
         });
+    }); 
+}
+
+function insertClass(request, response) {
+    console.log("inserting class");
+    //pull form data from body
+    var courseName = request.body.courseName;
+    var courseNumber = request.body.courseNumber;
+    var sectionNumber = request.body.sectionNumber;
+   
+    var client = new pg.Client(connectionString);
+    
+    client.connect(function(err){
+        if (err) {
+            console.log("Error connecting to database");
+            console.log(err);
+            callback(err, null);
+        }
+        
+        var sql = "INSERT INTO class(name, class_code, section, teacher_id) VALUES($1::text, $2::text, $3::text, $4::int);"
+        var params = [courseName, courseNumber, sectionNumber, 1 /*temporary until we know how to get the teacher_id*/];
+        
+        var query = client.query(sql, params, function(err, result){
+            client.end(function(err) {
+                if (err) throw err;
+            });
+            
+            if (err) {
+                console.log("Error in query");
+                console.log(err);
+                callback(err, null);
+            }
+            
+            response.redirect('/viewclasses.html');
+        });
+        
     });
 }
+
+
